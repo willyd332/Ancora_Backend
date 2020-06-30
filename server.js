912a6b62d60d91364b9ee01c7935e647571d45bb
will <sopwill3@gmail.com>
2020-06-30 10:11:01 -0600

    basic Express server set up

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const session = require('express-session');
+require('dotenv').config();
+require('./db/db');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const corsOptions = {
+  origin: process.env.FRONTEND_URL, // allowed address
+  credentials: true, // allows session cookies to be sent
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  'Access-Control-Allow-Origin': process.env.SESSION_SECRET,
+}));
+
+const trialController = require('./controllers/trial-controller');
+
+app.use('/trial', trialController);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(process.env.PORT || 9000, () => {
+  console.log(`Server is listening on port ${process.env.PORT} or 9000`);
+});
